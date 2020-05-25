14e4bef82cc2af3c805cbe3fd2d7855775d9a9e7
xyctruth <398041993@qq.com>
2020-05-25 14:23:08 +0800

    add:test

---
+++ src/main/java/ai/ones/sonarqube/controllers/TestController.java
@@ -0,0 +1,25 @@
+package ai.ones.sonarqube.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sun.security.util.Password;
+
+import javax.xml.ws.Action;
+
+/**
+ * TODO
+ *
+ * @author xyctruth
+ * @date 2020-05-25 ~ 12:22
+ */
+@RestController
+public class TestController {
+
+    @RequestMapping("/hi")
+    public String Hi(){
+        String UserName="123";
+        String PASSWORD="123";
+        return UserName+ "--------"+PASSWORD;
+    }
+}
